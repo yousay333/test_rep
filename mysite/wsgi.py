df0e9f0ec3a10928cb63c65118a430205bd84a1d
yousay333 <42889046+yousay333@users.noreply.github.com>
2018-09-07 01:27:32 +0000

    Update wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,20 +1,8 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
 
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myDjango.settings")
 
-application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
-
+application = Cling(get_wsgi_application())
