2e49ec40da887a1cfd08bed6b664ae22e701bf54
yousay333 <yousay333@yahoo.co.jp>
2018-09-17 23:47:40 +0900

    template ex

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import include, url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+
